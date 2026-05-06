9953891ee72164c0f5493197fdbf366403219c4e
Ray <30580339+uigleki@users.noreply.github.com>
2026-05-06 21:07:12 +0800

    chore: upgrade dev environment with flake-parts and pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -1,18 +1,41 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nodejs
-          pnpm
-        ];
-      };
+    inputs@{ flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.git-hooks.flakeModule ];
+      systems = import systems;
+
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ config.pre-commit.devShell ];
+
+            packages = with pkgs; [
+              nodejs
+              pnpm
+            ];
+          };
+
+          pre-commit.settings = {
+            hooks.convco.enable = true;
+            package = pkgs.prek;
+          };
+        };
     };
 }
